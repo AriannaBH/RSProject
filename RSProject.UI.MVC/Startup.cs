15e66131204339abbee25fa6e931feba3d168c0e
AriannaBH <abh223d@gmail.com>
2020-11-10 16:22:41 -0600

    Initial Commit

---
+++ RSProject.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RSProject.UI.MVC.Startup))]
+namespace RSProject.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
